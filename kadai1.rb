7c029731f1c480c5d3fb44f82a0f1a780c61d071
NAKAMURA Mai <mai@iot-25>
2017-11-21 10:15:35 +0900

    2017 -11 -21 -mai -second

--- kadai1.rb
+++ kadai1.rb
@@ -1,9 +1,9 @@
 for i in 1..100
 	if i%3==0&&i%5==0 then
-		print("FizzBuzz\n")
+		printf("FizzBuzz\n")
 	elsif i%3==0 then
-		print("Buzz\n")
+		printf("Buzz\n")
 	else
-		print("%d\n",i)
+		printf("%d\n",i)
 	end
 end
