997b4be80ddb586cfae6224a63b8a14b5ab13517
NAKAMURA Mai <mai@iot-25>
2017-12-05 17:23:35 +0900

    fizzubuzz7

--- bin/fizzbuzz.rb
+++ bin/fizzbuzz.rb
@@ -3,6 +3,8 @@ for i in 1..100
                 printf("FizzBuzz\n")
         elsif i%3==0 then
                 printf("Buzz\n")
+	elsif i%7==0 then
+		printf("git\n")
         else
                 printf("%d\n",i)
         end
